8547bfee814504713635977bfefcaf517c461caf
przemyslawbak <przemyslaw.bak@simple-mail.net>
2019-08-09 07:42:46 +0200

    First commit

---
+++ G_Hoover.Init/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace G_Hoover.Init
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var startupView = new Views.BrowserView();
+            startupView.ShowDialog();
+
+            base.OnStartup(e);
+        }
+    }
+}
